ab67d0e548834822f1e041baf3f8ae0a434bf3b3
Diego <145619634+diegoxavir@users.noreply.github.com>
2025-02-02 01:16:52 -0500

    Added more json functionality
    Added more json functionality

--- js/main.js
+++ js/main.js
@@ -9,7 +9,36 @@ function getNames() {
     .then(response => response.json())
     .then(function(response) {
         console.log(response);
+        const names = response.results
+        const ul = document.createElement("ul");
+        names.forEach(name => {
+            const li = document.createElement("li");
+            li.classList.add("name");
+            const a = document.createElement("a");
+            a.textContent = name["name"];
+            a.dataset.movie = name["films"][0];
+            li.appendChild(a);
+            ul.appendChild(li);
+        })
+        characterBox.appendChild(ul);
     })
+
+    .then(function() {
+        const links = document.querySelectorAll("#character-box li a");
+        links.forEach(function(links){
+            links.addEventListener("click", getMovie);
+        })
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+
+
+
+}
+
+function getMovie(e) {
+    comsole.log("movie called");
 }
 
 
