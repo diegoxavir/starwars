70c19acc992ef20b722832fae991a1fb2b92e177
casspb <145030622+casspb@users.noreply.github.com>
2025-02-04 14:18:23 -0500

    added loader for movie poster
    added loader for movie posters and movie info after the characters clicked

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 const characterBox = document.querySelector("#character-box");
 const infoTemplate = document.querySelector("#info-template");
 const infoCon = document.querySelector(".info-con");
+const loader = document.querySelector("#loader");
 const baseUrl = "https://swapi.dev/api/";
 
 function getNames() {
+
+    
     fetch(`${baseUrl}people`)
     .then(response => response.json())
     .then(function(response) {
@@ -34,20 +37,28 @@ function getNames() {
     })
     .catch(function(err) {
         console.log(err);
+
+        const errorMsg = document.createElement("p");
+        errorMsg.textContent = "looks like something went wrong. Check your connection, you may be in a different galaxy"
+        const container = document.querySelectorAll(".error-con");
+
+        container.appendChild(errorMsg);
+
     })
 
 
 
 }
 function getFilms(e) {
-    console.log("films's called");
-
     const filmUrls = e.currentTarget.dataset.movie;
 
+    // Clear previous content
     infoCon.innerHTML = "";
 
     if (filmUrls) {
+        loader.classList.remove("hidden");
         const urls = filmUrls.split(",");
+
         urls.forEach(url => {
             fetch(url)
                 .then(response => response.json())
@@ -56,20 +67,28 @@ function getFilms(e) {
                     const movieHeading = clone.querySelector(".movie-heading");
                     const titleCrawl = clone.querySelector(".title-crawl");
                     const moviePoster = clone.querySelector(".movie-poster");
+
                     movieHeading.innerHTML = response.title;
-                    titleCrawl.innerHTML = response.opening_crawl; 
-            
+                    titleCrawl.innerHTML = response.opening_crawl;
+
+                    // Add the movie info to the container
                     infoCon.appendChild(clone);
                 })
                 .catch(function (err) {
                     infoCon.innerHTML = `<p> No info available for this character </p>`;
                     console.log(err);
                 })
-        })
+                .finally(() => {
+                    // Hide the loader after all requests have finished
+                    loader.classList.add("hidden");
+                });
+        });
     } else {
         infoCon.innerHTML = `<p> No films available for this character </p>`;
+        loader.classList.add("hidden");
     }
 }
+
 
 getNames();
 })();
